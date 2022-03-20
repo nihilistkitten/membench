e6cb5ee767eb43dbb06674a7fcbff479e6e7dc91
Riley Shahar <riley.shahar@gmail.com>
2022-03-19 20:21:33 -0700

    Fetch ten random offsets at once

--- bench.c
+++ bench.c
@@ -6,19 +6,21 @@
 
 double bench(size_t buffer_size, size_t niters) {
   struct timespec start, stop;
-  double time_elapsed, mean_time;
-  size_t counter, offset;
+  size_t counter;
   data_t *buffer;
-  double *times;
+  size_t *offsets;
+  double time, time_elapsed;
   volatile data_t _
       __attribute__((unused)); // volatile prevents dead code optimization,
                                // confirmed by checking the assembly. attribute
                                // unused prevents compiler warnings.
 
-  buffer = calloc(buffer_size, sizeof(data_t));
-  times = calloc(niters, sizeof(double));
+  time = 0;
 
-  if (!buffer || !times) {
+  buffer = calloc(buffer_size, sizeof(data_t));
+  offsets = calloc(10, sizeof(size_t));
+
+  if (!buffer || !offsets) {
     fprintf(stderr, "Memory allocation failed!\n");
     exit(1);
   }
@@ -28,25 +30,39 @@ double bench(size_t buffer_size, size_t niters) {
   }
 
   for (counter = 0; counter < niters; counter++) {
-    offset = rand() % buffer_size;
+    offsets[0] = rand() % buffer_size;
+    offsets[1] = rand() % buffer_size;
+    offsets[2] = rand() % buffer_size;
+    offsets[3] = rand() % buffer_size;
+    offsets[4] = rand() % buffer_size;
+    offsets[5] = rand() % buffer_size;
+    offsets[6] = rand() % buffer_size;
+    offsets[7] = rand() % buffer_size;
+    offsets[8] = rand() % buffer_size;
+    offsets[9] = rand() % buffer_size;
 
     clock_gettime(CLOCK_MONOTONIC, &start);
-    _ = buffer[offset];
+    _ = buffer[offsets[0]];
+    _ = buffer[offsets[1]];
+    _ = buffer[offsets[2]];
+    _ = buffer[offsets[3]];
+    _ = buffer[offsets[4]];
+    _ = buffer[offsets[5]];
+    _ = buffer[offsets[6]];
+    _ = buffer[offsets[7]];
+    _ = buffer[offsets[8]];
+    _ = buffer[offsets[9]];
     clock_gettime(CLOCK_MONOTONIC, &stop);
 
     time_elapsed =
         (stop.tv_sec - start.tv_sec) * 1e9 + (stop.tv_nsec - start.tv_nsec);
-    times[counter] = time_elapsed;
+    time = time + time_elapsed / 10;
   }
 
-  mean_time = 0;
-  for (counter = 0; counter < niters; counter++) {
-    mean_time += times[counter];
-  }
-  mean_time = mean_time / niters;
+  time = time / niters;
 
   free(buffer);
-  free(times);
+  free(offsets);
 
-  return mean_time;
+  return time;
 }
